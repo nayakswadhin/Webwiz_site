f93689d2e2d84bca1b4652c402795a30a7dbb791
Swadhin <nayak.swadhin25@gmail.com>
2023-05-14 22:08:13 +0530

    First Commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSW5DOwqvguTqyTSwsL925J20WB7voVM8",
+    authDomain: "webwiz-login.firebaseapp.com",
+    projectId: "webwiz-login",
+    storageBucket: "webwiz-login.appspot.com",
+    messagingSenderId: "961298226257",
+    appId: "1:961298226257:web:877ff176f1107282a16c70",
+    measurementId: "G-MLJV93P1GV"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ welcome.js
@@ -0,0 +1,8 @@
+firebase.auth().onAuthStateChanged((user)=>{
+    if(!user){
+        location.replace("login.html")
+    }
+    else{
+        document.getElementById('user').innerHTML="Hello"+ user.email
+    }
+})
