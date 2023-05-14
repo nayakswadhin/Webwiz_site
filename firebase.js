ce009b3fc5e0b96206588845c3f6b20a14555c65
Swadhin Kumar Nayak <114871538+nayakswadhin@users.noreply.github.com>
2023-05-15 01:45:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,23 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDSW5DOwqvguTqyTSwsL925J20WB7voVM8",
-    authDomain: "webwiz-login.firebaseapp.com",
-    projectId: "webwiz-login",
-    storageBucket: "webwiz-login.appspot.com",
-    messagingSenderId: "961298226257",
-    appId: "1:961298226257:web:877ff176f1107282a16c70",
-    measurementId: "G-MLJV93P1GV"
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSW5DOwqvguTqyTSwsL925J20WB7voVM8",
+  authDomain: "webwiz-login.firebaseapp.com",
+  projectId: "webwiz-login",
+  storageBucket: "webwiz-login.appspot.com",
+  messagingSenderId: "961298226257",
+  appId: "1:961298226257:web:877ff176f1107282a16c70",
+  measurementId: "G-MLJV93P1GV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export {auth}
+export{onAuthStateChanged}
